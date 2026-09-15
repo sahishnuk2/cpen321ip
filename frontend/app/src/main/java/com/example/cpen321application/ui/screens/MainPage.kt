3dddb80f1384f5135b11eccd90543ef0574af343
Sahishnu <sahishnuk2@gmail.com>
2026-09-15 11:54:02 -0700

    finish buttons on main page

--- frontend/app/src/main/java/com/example/cpen321application/ui/screens/MainPage.kt
+++ frontend/app/src/main/java/com/example/cpen321application/ui/screens/MainPage.kt
@@ -4,33 +4,63 @@ import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.Button
+import androidx.compose.material3.ButtonDefaults
+import androidx.compose.ui.graphics.Color
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.draw.blur
+import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import com.example.cpen321application.ui.theme.CPEN321ApplicationTheme
 
 @Composable
 fun MainPage() {
     Column(
         modifier = Modifier.fillMaxSize(),
         verticalArrangement = Arrangement.spacedBy(
-            16.dp,
+            100.dp,
             Alignment.CenterVertically
         ),
         horizontalAlignment = Alignment.CenterHorizontally
     ) {
-        Button(onClick = {}) {
-            Text("Login")
-        }
-        Button(onClick = {}) {
-            Text("PixelArt")
-        }
-        Button(onClick = {}) {
-            Text("Timer")
-        }
+        MainMenuButton("Login")
+        MainMenuButton("Pixel Art")
+        MainMenuButton("Timer")
     }
 
 }
+
+@Composable
+private fun MainMenuButton(text: String) {
+    Button(
+        onClick = {},
+        modifier = Modifier.size(
+            width = 200.dp,
+            height = 75.dp
+        ),
+        shape = RoundedCornerShape(20.dp),
+        colors = ButtonDefaults.buttonColors(
+            containerColor = Color.Blue,
+            contentColor = Color.White
+        )
+
+    ) {
+        Text(
+            text = text,
+            fontSize = 30.sp
+        )
+    }
+}
+
+@Preview(showBackground = true)
+@Composable
+private fun MainPagePreview() {
+    CPEN321ApplicationTheme {
+        MainPage()
+    }
+}
+
